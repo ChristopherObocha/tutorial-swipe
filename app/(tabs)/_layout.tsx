30a061fdb58f7b6b40c8c1b2d5ed354e0c7343ba
Christopher Obocha <christopherobocha@gmail.com>
2024-08-31 21:53:31 +0100

    added a new screen for testing

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,6 +32,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="test"
+        options={{
+          title: 'Test',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'terminal' : 'terminal-outline'} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }

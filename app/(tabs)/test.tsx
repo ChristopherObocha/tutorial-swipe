4ee77a219b8f79a4972ef4b277b3723081b85f89
Christopher Obocha <christopherobocha@gmail.com>
2024-09-08 17:33:04 +0100

    remove unused code and comments

--- app/(tabs)/test.tsx
+++ app/(tabs)/test.tsx
@@ -1,45 +1,11 @@
-import { Image, StyleSheet, Platform, TouchableHighlight, Text } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-//@ts-ignore
-import Swipeable from 'react-native-swipeable';
 
 import SwipeButton from '@/components/SwipeButton';
 
 export default function TestScreen() {
-  // const leftContent = <ThemedText type="subtitle">Pull to activate</ThemedText>;
-
-  // const rightButtons = [
-  //   <TouchableHighlight><ThemedText type="subtitle">Button 1</ThemedText></TouchableHighlight>,
-  //   <TouchableHighlight><ThemedText type="subtitle">Button 2</ThemedText></TouchableHighlight>
-  // ];
-
-  // const MyListItem = () => {
-  //   return (
-  //     <Swipeable leftContent={leftContent} rightButtons={rightButtons}>
-  //       <ThemedText type="subtitle">My swipeable content</ThemedText>
-  //     </Swipeable>
-  //   );
-  // };
-  const leftContent = <Text>Pull to activate</Text>;
-
-  const rightButtons = [
-    <TouchableHighlight><Text>Button 1</Text></TouchableHighlight>,
-    <TouchableHighlight><Text>Button 2</Text></TouchableHighlight>
-  ];
-
-  const MyListItem = () => {
-    return (
-      <Swipeable leftContent={leftContent} rightButtons={rightButtons}>
-        <Text>My swipeable content</Text>
-      </Swipeable>
-    );
-  }
-
-
   return (
     <>
        <ParallaxScrollView
